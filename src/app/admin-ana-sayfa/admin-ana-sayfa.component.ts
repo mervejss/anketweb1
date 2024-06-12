61293bea5a068acf6714c5caa71d3a6670e128f4
aumervejss <72447846+mervejss@users.noreply.github.com>
2024-06-13 02:36:58 +0300

    anketweb1

--- src/app/admin-ana-sayfa/admin-ana-sayfa.component.ts
+++ src/app/admin-ana-sayfa/admin-ana-sayfa.component.ts
@@ -47,4 +47,9 @@ kullanicilaraAnketSec() {
 adminRehber() {
   this.aktifSayfa = 'admin-rehber'; // Başında / olmadan rotayı belirtin
   }
+  
+  adminBildirim() {
+    this.aktifSayfa = 'admin-bildirimleri'; // Başında / olmadan rotayı belirtin
+    }
+
 }
---
+++ src/app/admin-bildirimleri/admin-bildirimleri.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-bildirimleri',
+  templateUrl: './admin-bildirimleri.component.html',
+  styleUrls: ['./admin-bildirimleri.component.scss']
+})
+export class AdminBildirimleriComponent {
+
+}
